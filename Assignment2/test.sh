2019612fad1cbaab80cbd098cc545cfb535e0a89
Jerry Yang <1444781838@qq.com>
2021-05-13 15:48:12 +0800

    ass2 done

---
+++ Assignment2/test.sh
@@ -0,0 +1,15 @@
+gcc main.c -O2 -lpthread -o mm
+for size in 16 128 1024 2048 4096 8192
+do
+    echo "##########################" $size "##########################"
+
+    python gm.py $size $size "Amat$size.txt"
+    python gm.py $size $size "Bmat$size.txt"
+    for nt in 1 2 4 8
+    do
+        echo "mm -a Amat$size.txt -b Bmat$size.txt -t $nt"
+
+        ./mm -a Amat$size.txt -b Bmat$size.txt -t $nt
+    done
+done
+echo "[DONE.]"
